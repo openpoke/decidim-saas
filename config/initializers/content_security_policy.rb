d07906349dbbc75fe34817f014b37d7f152461c5
Ivan Vergés <ivan@pokecode.net>
2025-02-06 17:17:09 +0100

    sets default security polcies from env

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -1,2 +1,14 @@
 # For tuning the Content Security Policy, check the Decidim documentation site
 # https://docs.decidim.org/develop/en/customize/content_security_policy
+Decidim.configure do |config|
+  config.content_security_policies_extra = {
+    "connect-src" => ENV["CONTENT_SECURITY_POLICY"].split,
+    "img-src" => ENV["CONTENT_SECURITY_POLICY"].split,
+    "default-src" => ENV["CONTENT_SECURITY_POLICY"].split,
+    "script-src" => ENV["CONTENT_SECURITY_POLICY"].split,
+    "style-src" => ENV["CONTENT_SECURITY_POLICY"].split,
+    "font-src" => ENV["CONTENT_SECURITY_POLICY"].split,
+    "frame-src" => ENV["CONTENT_SECURITY_POLICY"].split,
+    "media-src" => ENV["CONTENT_SECURITY_POLICY"].split
+  }
+end
