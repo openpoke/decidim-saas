83f0b0edea9fac355ad28ed6e6acfcd32e1c2685
Ivan Vergés <ivan@pokecode.net>
2025-02-06 17:23:13 +0100

    fix empty env

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -2,13 +2,13 @@
 # https://docs.decidim.org/develop/en/customize/content_security_policy
 Decidim.configure do |config|
   config.content_security_policies_extra = {
-    "connect-src" => ENV["CONTENT_SECURITY_POLICY"].split,
-    "img-src" => ENV["CONTENT_SECURITY_POLICY"].split,
-    "default-src" => ENV["CONTENT_SECURITY_POLICY"].split,
-    "script-src" => ENV["CONTENT_SECURITY_POLICY"].split,
-    "style-src" => ENV["CONTENT_SECURITY_POLICY"].split,
-    "font-src" => ENV["CONTENT_SECURITY_POLICY"].split,
-    "frame-src" => ENV["CONTENT_SECURITY_POLICY"].split,
-    "media-src" => ENV["CONTENT_SECURITY_POLICY"].split
+    "connect-src" => ENV.fetch("CONTENT_SECURITY_POLICY","").split,
+    "img-src" => ENV.fetch("CONTENT_SECURITY_POLICY","").split,
+    "default-src" => ENV.fetch("CONTENT_SECURITY_POLICY","").split,
+    "script-src" => ENV.fetch("CONTENT_SECURITY_POLICY","").split,
+    "style-src" => ENV.fetch("CONTENT_SECURITY_POLICY","").split,
+    "font-src" => ENV.fetch("CONTENT_SECURITY_POLICY","").split,
+    "frame-src" => ENV.fetch("CONTENT_SECURITY_POLICY","").split,
+    "media-src" => ENV.fetch("CONTENT_SECURITY_POLICY","").split
   }
 end
