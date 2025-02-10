72ab81181ccba7fa5edff3278f5c101678d4c6d0
Ivan Vergés <ivan@pokecode.net>
2025-02-10 22:18:53 +0100

    ignore routing errors

--- config/initializers/rorvswild.rb
+++ config/initializers/rorvswild.rb
@@ -1,1 +1,7 @@
-RorVsWild.start(api_key: ENV["RORVSWILD_API_KEY"]) if ENV["RORVSWILD_API_KEY"].present?
+RorVsWild.start(
+  api_key: ENV["RORVSWILD_API_KEY"],
+  ignored_exceptions: [
+    "ActionController::RoutingError",
+    "ActiveRecord::RecordNotFound"
+  ]
+) if ENV["RORVSWILD_API_KEY"].present?
