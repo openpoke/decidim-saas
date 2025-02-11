ddc354a3b364b5a1d89d209d80f73d1528a6b0f8
Ivan Vergés <ivan@pokecode.net>
2025-02-11 12:32:02 +0100

    ignore missing template

--- config/initializers/rorvswild.rb
+++ config/initializers/rorvswild.rb
@@ -2,6 +2,7 @@ RorVsWild.start(
   api_key: ENV["RORVSWILD_API_KEY"],
   ignored_exceptions: [
     "ActionController::RoutingError",
-    "ActiveRecord::RecordNotFound"
+    "ActiveRecord::RecordNotFound",
+    "ActionView::MissingTemplate"
   ]
 ) if ENV["RORVSWILD_API_KEY"].present?
