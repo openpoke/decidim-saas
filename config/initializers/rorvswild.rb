56bdb6ba61c6d35e4bfd434eea135e298f1972e9
Ivan Vergés <ivan@pokecode.net>
2025-02-11 11:22:25 +0100

    ignore S3 errors

--- config/initializers/rorvswild.rb
+++ config/initializers/rorvswild.rb
@@ -2,6 +2,7 @@ RorVsWild.start(
   api_key: ENV["RORVSWILD_API_KEY"],
   ignored_exceptions: [
     "ActionController::RoutingError",
-    "ActiveRecord::RecordNotFound"
+    "ActiveRecord::RecordNotFound",
+    "Aws::S3::Errors::InvalidRequest"
   ]
 ) if ENV["RORVSWILD_API_KEY"].present?
