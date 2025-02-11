2df87581e6a9d34e734435aba3b10fb37216eeb6
Ivan Vergés <ivan@pokecode.net>
2025-02-11 12:01:28 +0100

    add S3 cloudflare config for checksums

--- config/initializers/rorvswild.rb
+++ config/initializers/rorvswild.rb
@@ -2,7 +2,6 @@ RorVsWild.start(
   api_key: ENV["RORVSWILD_API_KEY"],
   ignored_exceptions: [
     "ActionController::RoutingError",
-    "ActiveRecord::RecordNotFound",
-    "Aws::S3::Errors::InvalidRequest"
+    "ActiveRecord::RecordNotFound"
   ]
 ) if ENV["RORVSWILD_API_KEY"].present?
