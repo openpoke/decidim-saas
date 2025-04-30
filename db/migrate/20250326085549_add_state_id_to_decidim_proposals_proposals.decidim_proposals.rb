a4975701752b88b89485f7b856da16f004417bdd
Anna Topalidi <60363870+antopalidi@users.noreply.github.com>
2025-04-30 10:59:15 +0200

    Upgrade to 0.29 (#79)
    * rebase
    
    * fix Gemfile
    
    * change CI
    
    * change CI
    
    * remove elections
    
    * fix admin_custom_iframe_spec
    
    * change test
    
    * fix dockerfile
    
    * fix dockerfile
    
    * fix index length
    
    ---------
    
    Co-authored-by: Ivan Vergés <ivan@pokecode.net>

---
+++ db/migrate/20250326085549_add_state_id_to_decidim_proposals_proposals.decidim_proposals.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+# This migration comes from decidim_proposals (originally 20240110203502)
+class AddStateIdToDecidimProposalsProposals < ActiveRecord::Migration[6.1]
+  def up
+    add_column :decidim_proposals_proposals, :decidim_proposals_proposal_state_id, :integer
+    add_index :decidim_proposals_proposals, :decidim_proposals_proposal_state_id, name: "index_decidim_proposals_on_decidim_proposal_state_id"
+    add_foreign_key :decidim_proposals_proposals, :decidim_proposals_proposal_states, column: :decidim_proposals_proposal_state_id
+  end
+
+  def down
+    remove_foreign_key :decidim_proposals_proposals, column: :decidim_proposals_proposal_state_id
+    remove_column :decidim_proposals_proposals, :decidim_proposals_proposal_state_id
+  end
+end
