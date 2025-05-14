dd5fa4912ead9e043bb888067e0b26ad77e39934
Ivan Vergés <ivan@pokecode.net>
2025-05-14 10:08:42 +0200

    fix specs

--- decidim-saas-som_mobilitat/spec/som_mobilitat_module_spec.rb
+++ decidim-saas-som_mobilitat/spec/som_mobilitat_module_spec.rb
@@ -4,10 +4,10 @@ require "rails_helper"
 
 describe "SomMobilitatModule" do
   it "defines the SomMobilitat module" do
-    expect(Saas::SomMobilitat).to be_a(Module)
+    expect(Decidim::Saas::SomMobilitat).to be_a(Module)
   end
 
   it "defines the SomMobilitat::Engine class as a subclass of Rails::Engine" do
-    expect(Saas::SomMobilitat::Engine < Rails::Engine).to be true
+    expect(Decidim::Saas::SomMobilitat::Engine < Rails::Engine).to be true
   end
 end
