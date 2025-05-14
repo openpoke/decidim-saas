85b8e41d0a81bb776fe5e4d426c432599cb4569b
Ivan Vergés <ivan@pokecode.net>
2025-05-14 10:26:58 +0200

    fix specs

--- decidim-saas-clean_clothes/spec/clean_clothes_module_spec.rb
+++ decidim-saas-clean_clothes/spec/clean_clothes_module_spec.rb
@@ -4,7 +4,7 @@ require "rails_helper"
 
 describe "CleanClothesModule" do
   it "defines the CleanClothes module" do
-    expect(Saas::CleanClothes).to be_a(Module)
+    expect(Decidim::Saas::CleanClothes).to be_a(Module)
   end
 
   it "defines the ExtraUserFields module" do
@@ -12,6 +12,6 @@ describe "CleanClothesModule" do
   end
 
   it "defines the CleanClothes::Engine class as a subclass of Rails::Engine" do
-    expect(Saas::CleanClothes::Engine < Rails::Engine).to be true
+    expect(Decidim::Saas::CleanClothes::Engine < Rails::Engine).to be true
   end
 end
