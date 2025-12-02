b87f55488bb0f47804840d4e84065c311e50fde6
Anna Topalidi <t.opalidisanna@gmail.com>
2025-12-02 11:53:19 +0100

    disable extra_user_fields tests

--- spec/saas_modules_spec.rb
+++ spec/saas_modules_spec.rb
@@ -11,13 +11,14 @@ describe "SaaSModules" do
     expect(defined?(Decidim::Saas::SomMobilitat)).to be_nil
   end
 
-  if ENV["WITH_EXTRA_USER_FIELDS"].present?
-    it "defines the ExtraUserFields module" do
-      expect(Decidim::ExtraUserFields).to be_a(Module)
-    end
-  else
-    it "does not define the ExtraUserFields module" do
-      expect(defined?(Decidim::ExtraUserFields)).to be_nil
-    end
+  # TODO: Uncomment when decidim-extra_user_fields is updated for v0.31
+  # if ENV["WITH_EXTRA_USER_FIELDS"].present?
+  #   it "defines the ExtraUserFields module" do
+  #     expect(Decidim::ExtraUserFields).to be_a(Module)
+  #   end
+  # else
+  it "does not define the ExtraUserFields module" do
+    expect(defined?(Decidim::ExtraUserFields)).to be_nil
   end
+  # end
 end
