ebfa072b40a6726d0e8a0e3adbc1dce7fa2c54a1
Ivan Vergés <ivan@pokecode.net>
2025-12-17 11:27:39 +0100

    add certificates

--- decidim-saas-ehu_agora/lib/decidim/saas/ehu_agora/engine.rb
+++ decidim-saas-ehu_agora/lib/decidim/saas/ehu_agora/engine.rb
@@ -18,9 +18,10 @@ module Decidim
                        sp_entity_id: ENV.fetch("SAML_SP_ENTITY_ID", nil),
                        idp_sso_service_url: ENV.fetch("SAML_IDP_SSO_SERVICE_URL", nil),
                        idp_sso_service_binding: "urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect",
-                       idp_cert: ENV.fetch("SAML_IDP_CERT", nil),
-                       idp_cert_fingerprint: ENV.fetch("SAML_IDP_CERT_FINGERPRINT", nil),
-                       name_identifier_format: "urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress"
+                       idp_cert: "-----BEGIN CERTIFICATE-----\n#{ENV.fetch("SAML_IDP_CERT", nil)}\n-----END CERTIFICATE-----",
+                       name_identifier_format: "urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress",
+                       certificate: "-----BEGIN CERTIFICATE-----\n#{ENV.fetch("SAML_SP_CERTIFICATE", nil)}\n-----END CERTIFICATE-----",
+                       private_key: "-----BEGIN PRIVATE KEY-----\n#{ENV.fetch("SAML_SP_PRIVATE_KEY", nil)}\n-----END PRIVATE KEY-----"
             end
             # Force Decidim to look at this provider if not defined in secrets.yml
             Rails.application.secrets[:omniauth][:saml] = {
