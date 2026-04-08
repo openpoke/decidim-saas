82050bf7feed1263cff5ca68dba800e62df795c6
elviabth <ejbenedith@gmail.com>
2026-04-08 13:17:51 +0200

    add mandatory field

--- decidim-saas-vivaio_delle_idee/lib/decidim/saas/vivaio_delle_idee/engine.rb
+++ decidim-saas-vivaio_delle_idee/lib/decidim/saas/vivaio_delle_idee/engine.rb
@@ -8,6 +8,17 @@ module Decidim
 
         paths["db/migrate"] = nil
         paths["lib/tasks"] = nil
+
+        initializer "saas.vivaio_delle_idee.municipality_region_province" do
+          Decidim::ExtraUserFields.configure do |config|
+            config.select_fields = {
+              municipality_region_province: {
+                "milan_lombardy" => "decidim.extra_user_fields.municipality_region_province.milan_lombardy",
+                "rome_lazio" => "decidim.extra_user_fields.municipality_region_province.rome_lazio"
+              }
+            }
+          end
+        end
       end
     end
   end
