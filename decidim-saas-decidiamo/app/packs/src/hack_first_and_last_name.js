537db4b430e982de323dd35269ea7c0c40e9133e
elviabth <ejbenedith@gmail.com>
2026-05-13 23:23:58 +0200

    fix js hack bug

--- decidim-saas-decidiamo/app/packs/src/hack_first_and_last_name.js
+++ decidim-saas-decidiamo/app/packs/src/hack_first_and_last_name.js
@@ -1,8 +1,9 @@
 function bindNameSync() {
   const firstNameInput = document.querySelector("#registration_user_text_fields_first_name");
   const lastNameInput = document.querySelector("#registration_user_text_fields_last_name");
-  
   const hiddenNameInput = document.querySelector("#registration_user_name_hidden");
+
+  if (!firstNameInput || !lastNameInput || !hiddenNameInput) return;
 
   firstNameInput.addEventListener("input", syncName);
   lastNameInput.addEventListener("input", syncName);
