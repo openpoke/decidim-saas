41f6cbe884738d451c5a5574c7d5b65d0434a423
Anna Topalidi <t.opalidisanna@gmail.com>
2026-09-18 12:06:36 +0200

    Align environment configs with Rails 8.1 defaults

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -10,9 +10,8 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  # Turn false under Spring and add config.action_view.cache_template_loading = true.
-  config.cache_classes = false
-  config.action_view.cache_template_loading = true
+  # While tests run files are not watched, reloading is not necessary.
+  config.enable_reloading = false
 
   # Eager loading loads your whole application. When running a single test locally,
   # this probably isn't necessary. It's a good idea to do in a continuous integration
@@ -31,7 +30,10 @@ Rails.application.configure do
   config.cache_store = :null_store
 
   # Raise exceptions instead of rendering exception templates.
-  config.action_dispatch.show_exceptions = false
+  config.action_dispatch.show_exceptions = :rescuable
+
+  # Raise error when a before_action's only/except options reference missing actions.
+  config.action_controller.raise_on_missing_callback_actions = true
 
   # Disable request forgery protection in test environment.
   config.action_controller.allow_forgery_protection = false
